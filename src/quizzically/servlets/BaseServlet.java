c96e1a08175de87a1e47fffcdaf5cb2076f0132d
Dominic Becker <dominic.r.becker@gmail.com>
2014-03-12 23:53:20 -0700

    move getInt, getString, etc from API servlet to BaseServlet

--- src/quizzically/servlets/BaseServlet.java
+++ src/quizzically/servlets/BaseServlet.java
@@ -26,4 +26,63 @@ public abstract class BaseServlet extends HttpServlet {
 		return new User(username);
 	}
 
+	protected int getInt(HttpServletRequest request, String field) 
+			throws ServletException {
+				return getInt(request, field, 0, true);
+	}
+
+	protected int getInt(HttpServletRequest request, String field, int defaultVal) 
+			throws ServletException {
+				return getInt(request, field, defaultVal, false);
+	}
+
+	protected String getString(HttpServletRequest request, String field)
+			throws ServletException {
+		return getString(request, field, null, true);
+	}
+
+	protected String getString(HttpServletRequest request, String field, String defaultVal) 
+			throws ServletException {
+		return getString(request, field, defaultVal, false);
+	}
+
+	protected String[] getArray(HttpServletRequest request, String field) 
+			throws ServletException {
+		String[] strs = request.getParameterValues(field);
+		if (strs == null) {
+			throw new ServletException("Required field '" + field + 
+					"' is missing");
+		}
+		return strs;
+	}
+
+	private int getInt(HttpServletRequest request, String field, int defaultVal, boolean required) 
+			throws ServletException {
+		int val = defaultVal;
+		try {
+			String str = getString(request, field, null, required);
+			if (str != null) {
+				val = Integer.parseInt(str);
+			}
+		} catch (NumberFormatException e) {
+			throw new ServletException("Expected integer for field '" +
+				 	field + "'");
+		}
+		return val;
+	}
+
+	private String getString(HttpServletRequest request, String field, String defaultVal, boolean required) 
+			throws ServletException {
+		String str = request.getParameter(field);
+		if (str == null) {
+			if (required) {
+				throw new ServletException("Required field '" + 
+						field + "' is missing");
+			} else {
+				return defaultVal;
+			}
+		}
+		return str;
+	}
+
 }
