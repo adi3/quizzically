cf6f576949a85fa2bef91b04b9c72c9d018b7a0b
nplatias <nplatias@stanford.edu>
2014-03-14 02:46:53 -0700

    add position to QuizAttempt

--- src/quizzically/models/QuizAttempt.java
+++ src/quizzically/models/QuizAttempt.java
@@ -5,27 +5,30 @@ import java.util.*;
 public class QuizAttempt extends Model {
 	private static final int NULL_VALUE = -1;
 	private static final String TABLE = "quiz_attempts";
-	private static final String[] QUIZ_ATTEMPTS_COLUMNS = {"created_at", "completed_at", "score", "quiz_id", "user_id"};
+	private static final String[] QUIZ_ATTEMPTS_COLUMNS = {"created_at", "completed_at", "score", "quiz_id", "user_id", "position"};
 	
 	private int quizId;
 	private int userId;
 	private Date createdAt;
 	private Date completedAt;
 	private int score;
+	private int position;
 
-	protected QuizAttempt(int id, int quizId, int userId, Date createdAt, Date completedAt, int score) {
+	protected QuizAttempt(int id, int quizId, int userId, Date createdAt, Date completedAt, 
+			int score, int position) {
 		super(id, TABLE, new QuizAttemptHydrator());
 		this.quizId = quizId;
 		this.userId = userId;
 		this.createdAt = createdAt;
 		this.completedAt = completedAt;
 		this.score = score;
+		this.position = position;
 	}
 
 	
 	public static QuizAttempt create(int quiz_id, int user_id) {
 		Date createdAt = new Date();
-		QuizAttempt qA = new QuizAttempt(NULL_VALUE, quiz_id, user_id, createdAt, null, NULL_VALUE);
+		QuizAttempt qA = new QuizAttempt(NULL_VALUE, quiz_id, user_id, createdAt, null, NULL_VALUE, 0);
 		qA.save(true); // dehydrates, inserts into DB and sets id to generated key
 		return qA;
 	}
@@ -56,6 +59,14 @@ public class QuizAttempt extends Model {
 	
 	public int score() {
 		return score;
+	}
+	
+	public int position() {
+		return position;
+	}
+	
+	public void setPosition(int position){
+		this.position = position;
 	}
 	
 	public void setCompletedAt(Date completedAt) {
